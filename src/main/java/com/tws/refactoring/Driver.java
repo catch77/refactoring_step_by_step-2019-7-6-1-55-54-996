cd626b69c1e88d27473636e9e6d2d941580318d7
catch77 <1297056296@qq.com>
2019-07-15 15:37:45 +0800

    Refactor: Move the check driver login to driver class

--- src/main/java/com/tws/refactoring/Driver.java
+++ src/main/java/com/tws/refactoring/Driver.java
@@ -1,9 +1,25 @@
 package com.tws.refactoring;
 
 public class Driver {
-    public int age;
+
+    private int age;
+
+    public Driver() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
 
     public Driver(int age) {
         this.age = age;
+    }
+
+    public boolean isLegalDriver() {
+        return this.age >= 18;
     }
 }
--- src/test/java/com/tws/refactoring/PoliceTest.java
+++ src/test/java/com/tws/refactoring/PoliceTest.java
@@ -2,35 +2,36 @@ package com.tws.refactoring;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PoliceTest {
 
-    private static Police police;
+    private static Driver driver;
 
-    @BeforeAll
-    public static void init() {
-        police = new Police();
+    @BeforeEach
+    public void init() {
+        driver = new Driver();
     }
 
     @Test
     public void should_return_true_when_driver_age_over_18() {
-        Driver driver = new Driver(19);
-        Assertions.assertSame(true, police.checkDriver(driver));
+        driver.setAge(19);
+        Assertions.assertSame(true, driver.isLegalDriver());
     }
 
     @Test
     public void should_false_true_when_driver_age_below_18() {
-        Driver driver = new Driver(15);
-        Assertions.assertSame(false, police.checkDriver(driver));
+        driver.setAge(15);
+        Assertions.assertSame(false, driver.isLegalDriver());
     }
 
     @Test
     public void should_false_true_when_driver_age_equal_18() {
-        Driver driver = new Driver(18);
-        Assertions.assertSame(true, police.checkDriver(driver));
+        driver.setAge(18);
+        Assertions.assertSame(true, driver.isLegalDriver());
     }
 
 }
